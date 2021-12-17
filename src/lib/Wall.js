73fb9a346cd9728d9415982f7197015e0d6887e8
David Federspiel <david@codefly.ninja>
2021-12-16 21:13:02 -0500

    refactoring

--- src/lib/Wall.js
+++ src/lib/Wall.js
@@ -10,33 +10,15 @@ export default class Wall {
     const halfWidth = this.w / 2;
     const halfHeight = this.h / 2;
 
-    this.coords = {
-      tl: {
-        x: this.x,
-        y: this.y,
-      },
-      tr: {
-        x: this.x + this.w,
-        y: this.y
-      },
-      bl: {
-        x: this.x,
-        y: this.y + this.h,
-      },
-      br: {
-        x: this.x + this.w,
-        y: this.y + this.h,
-      },
-    };
     this.angles = {
-        tl: this.#angle(this.coords.tl.x, this.coords.tl.y),
-        tr: this.#angle(this.coords.tr.x, this.coords.tr.y),
-        bl: this.#angle(this.coords.bl.x, this.coords.bl.y),
-        br: this.#angle(this.coords.br.x, this.coords.br.y),
+        tl: this.angle(this.x, this.y),
+        tr: this.angle(this.x + this.w, this.y),
+        bl: this.angle(this.x, this.y + this.h),
+        br: this.angle(this.x + this.w, this.y + this.h),
     };
   }
 
-  #angle(x, y) {
+  angle(x, y) {
     var dx = this.x + this.w / 2 - x;
     var dy = this.y + this.h / 2 - y;
 
