9628a722377d40b401c3b03b40adf71727ebdeb7
David Federspiel <david@federnet.com>
2022-01-04 20:53:26 -0500

    refactor

--- src/scenes/Supernovae/Supernovae.ts
+++ src/scenes/Supernovae/Supernovae.ts
@@ -1,57 +1,65 @@
-import { effects } from "../../lib/easing";
-import { calculate, rand } from "../../lib/helpers";
-import { Randomizable } from "../../lib/types";
-import Segment from "./Segment";
+import { easeInSine, effects } from '../../lib/easing';
+import { calculate, rand } from '../../lib/helpers';
+import Rect from '../../lib/Rect';
+import { Randomizable } from '../../lib/types';
+import Segment from './Segment';
 
 export default class Supernova implements Randomizable {
-  maxAlpha: number = 0;
-  alpha: number = 0;
+  maxAlpha: number = 0.8;
+  alpha: number = 1;
   direction: number = 0;
   rotationInterval: number = rand(-Math.PI / 60 / 60, Math.PI / 60 / 60);
   limit: number;
   angle: number = 0;
-  color: string = `rgb(${rand(125, 255)},${rand(125, 255)},${rand(125, 255)})`
+  color: string = `rgb(${rand(125, 255)},${rand(125, 255)},${rand(125, 255)})`;
   width = window.innerWidth;
-  height = window.innerHeight
-  lineWidth: number = .5
-  offset = rand(-50, 100)
-  steps: number = Math.floor(rand(50, 145))
-  ease: Function
-  segments: Segment[] = []
+  height = window.innerHeight;
+  lineWidth: number = 0.5;
+  offset = rand(-50, 100);
+  steps: number = Math.floor(rand(50, 145));
+  ease: Function;
+  segments: Segment[] = [];
   renderOutlines = false;
 
-  minModifier = rand(-2, 2)
+  minModifier = rand(-2, 2);
   maxModifier = rand(-2, 2);
 
   posX = 0;
   posY = 0;
 
-  private minRadius = rand(-50, 50)
-  private maxRadius = rand(50, 75)
+  ctx: CanvasRenderingContext2D;
 
-  constructor(angle: number, numPetals: number) {
-    this.limit = numPetals
+  private minRadius = rand(-50, 50);
+  private maxRadius = rand(50, 75);
+
+  constructor(
+    angle: number,
+    numPetals: number,
+    context: CanvasRenderingContext2D
+  ) {
+    this.limit = numPetals;
     this.angle = angle;
+    this.ctx = context;
     this.ease = effects[Math.floor(rand(0, effects.length))];
     this.randomize();
-    this.update(0)
+    this.update(0);
   }
 
   update(step: number) {
     if (step === 0) return;
     this.angle += step;
-    this.segments = []
-    this.maxRadius += this.maxModifier
-    this.minRadius += this.minModifier
+    this.segments = [];
+    this.maxRadius += this.maxModifier;
+    this.minRadius += this.minModifier;
 
     for (let c = 0; c < this.limit; c++) {
-      this.angle += ((Math.PI * 2) / this.limit);
+      this.angle += (Math.PI * 2) / this.limit;
       const { x: pX, y: pY } = calculate.getVertexFromAngle(
         this.width / 2,
         this.height / 2,
         this.angle,
         this.offset
-      )
+      );
       let p = new Segment(
         pX,
         pY,
@@ -60,20 +68,84 @@ export default class Supernova implements Randomizable {
         this.maxRadius,
         this.steps,
         this.ease
-      )
-      this.segments.push(p)
+      );
+      this.segments.push(p);
     }
+  }
+
+  private renderLines(objects: Rect[], alpha: number): void {
+    this.ctx.strokeStyle = this.color;
+    this.ctx.lineWidth = this.lineWidth;
+    this.ctx.beginPath();
+    objects.forEach((o, idx) => {
+      this.ctx.globalAlpha = alpha;
+      this.ctx.moveTo(this.width / 2, this.height / 2);
+      this.ctx.lineTo(o.x, o.y);
+    });
+    this.ctx.stroke();
+  }
+
+  private renderOutline(objects: Rect[], alpha: number): void {
+    if (objects.length === 0) return;
+    this.ctx.lineWidth = this.lineWidth * 2;
+    this.ctx.fillStyle = this.color;
+    this.ctx.globalAlpha = alpha;
+    objects.forEach((o, idx) => {
+      this.ctx.beginPath();
+      this.ctx.moveTo(o.x, o.y);
+      if (idx === objects.length - 1)
+        this.ctx.lineTo(objects[0].x, objects[0].y);
+      else this.ctx.lineTo(objects[idx + 1].x, objects[idx + 1].y);
+      this.ctx.stroke();
+    });
+  }
+
+  private renderArcs(objects: Rect[], alpha: number): void {
+    if (objects.length === 0) return;
+    this.ctx.globalAlpha = alpha;
+    this.ctx.fillStyle = this.color;
+    this.ctx.strokeStyle = 'black'
+    objects.forEach((o, idx) => {
+      this.ctx.beginPath();
+      const e = easeInSine(idx + 1, 1, o.w / 2, objects.length);
+      this.ctx.arc(o.x, o.y, e, 0, Math.PI * 2);
+      this.ctx.fill();
+      this.ctx.stroke();
+    });
   }
 
   randomize(): void {
     this.angle = 0;
-    this.minRadius = rand(5, 50)
-    this.maxRadius = rand(10, 50)
-    this.minModifier = rand(-.1, -1.5)
-    this.maxModifier = rand(-.5, -2.5);
-    this.limit = Math.floor(rand(2, 20))
-    this.steps = Math.floor(rand(10, 200 / this.limit))
-    this.offset = 0
-    this.rotationInterval = rand(-(Math.PI / 60), Math.PI / 60) / 20
+    this.minRadius = rand(5, 50);
+    this.maxRadius = rand(10, 50);
+    this.minModifier = rand(-0.1, -1.5);
+    this.maxModifier = rand(-0.5, -2.5);
+    this.limit = Math.floor(rand(2, 20));
+    this.steps = Math.floor(rand(10, 200 / this.limit));
+    this.offset = 0;
+    this.rotationInterval = rand(-(Math.PI / 60), Math.PI / 60) / 20;
+  }
+
+  render() {
+    this.segments.forEach((p) => {
+      // this.renderLines(
+      //   p.points.filter((i, idx) => idx % Math.floor(rand(3, 20)) === 0),
+      //   this.alpha / Math.floor(rand(5, 10))
+      // );
+
+      // if (this.renderOutlines) this.renderOutline(p.points, this.alpha);
+
+      this.renderArcs(
+        p.points.filter((i, idx) => idx % Math.floor(rand(1, 20)) === 0),
+        this.alpha
+      );
+    });
+    if (this.alpha > 0.8 && this.direction > 0) {
+      this.direction = -this.direction;
+    }
+    this.ctx.strokeStyle = this.color;
+    this.update(this.rotationInterval);
+    this.alpha += this.direction;
+    console.log(this.alpha);
   }
 }
--- src/scenes/Supernovae/SupernovaeScene.scene.ts
+++ src/scenes/Supernovae/SupernovaeScene.scene.ts
@@ -28,53 +28,15 @@ export default class SupernovaeScene implements Scene, Randomizable {
     //this.crystals.forEach(f => f.randomize())
   }
 
-  renderLines(objects: Rect[], alpha: number): void {
-    this.ctx.beginPath();
-    objects.forEach((o, idx) => {
-      this.ctx.globalAlpha = alpha
-      this.ctx.moveTo(this.width / 2, this.height / 2)
-      this.ctx.lineTo(o.x, o.y)
-    });
-    this.ctx.stroke();
-  }
-
-  renderOutline(objects: Rect[], alpha: number): void {
-    if (objects.length === 0) return;
-    this.ctx.globalAlpha = alpha
-    objects.forEach((o, idx) => {
-      this.ctx.beginPath()
-      this.ctx.moveTo(o.x, o.y)
-      if (idx === objects.length - 1)
-        this.ctx.lineTo(objects[0].x, objects[0].y)
-      else
-        this.ctx.lineTo(objects[idx + 1].x, objects[idx + 1].y)
-      this.ctx.stroke();
-    });
-  }
-
-  renderArcs(objects: Rect[], alpha: number): void {
-    if (objects.length === 0) return;
-    this.ctx.globalAlpha = alpha
-    objects.forEach((o, idx) => {
-      const e = easeInSine(idx + 1, 1, o.w / 2, objects.length)
-      this.ctx.beginPath()
-      this.ctx.arc(o.x, o.y, e, 0, Math.PI * 2)
-      this.ctx.fill()
-      this.ctx.stroke();
-    });
-  }
-
-
-
   render(): void {
 
     this.ctx.strokeStyle = 'white';
 
     if (this.count % this.displayDuration === 0) {
-      this.ctx.globalAlpha = 1
-      this.ctx.fillStyle = 'black'
-      this.ctx.fillRect(0, 0, this.width, this.height)
-      let f = new Supernova(this.angle, Math.floor(rand(2, 20)))
+      // this.ctx.globalAlpha = 1
+      // this.ctx.fillStyle = 'black'
+      // this.ctx.fillRect(0, 0, this.width, this.height)
+      let f = new Supernova(this.angle, Math.floor(rand(2, 20)), this.ctx)
       f.direction = this.maxOpacity / Math.floor(this.displayDuration / 2);
       f.maxAlpha = this.maxOpacity
       f.alpha = 0;
@@ -89,33 +51,13 @@ export default class SupernovaeScene implements Scene, Randomizable {
     }
 
     this.crystals?.forEach((c, idx) => {
-
-      c.segments.forEach(p => {
-        this.ctx.strokeStyle = c.color;
-        this.ctx.lineWidth = c.lineWidth
-        this.renderLines(p.points.filter((i, idx) => idx % Math.floor(rand(3, 20)) === 0), c.alpha / Math.floor(rand(5, 10)));
-        this.ctx.lineWidth = c.lineWidth * 2
-        this.ctx.fillStyle = c.color
-        if (c.renderOutlines) this.renderOutline(p.points, c.alpha);
-        this.renderArcs(p.points.filter((i, idx) => idx % Math.floor(rand(3, 8)) === 0), c.alpha)
-      })
+      c.render();
     })
 
-    this.crystals?.forEach(c => {
-      if (c.alpha > this.maxOpacity && c.direction > 0) {
-        c.direction = -c.direction
-      }
-      this.ctx.strokeStyle = c.color;
-      c.update(c.rotationInterval)
-      c.alpha += c.direction
-      console.log(c.alpha)
-    })
-
-    this.ctx.globalAlpha = 1 / (this.displayDuration / 20)
-    this.ctx.fillStyle = '#000'
-    this.ctx.fillRect(0, 0, this.width, this.height)
-    this.ctx.globalAlpha = 1;
-    this.ctx.fillRect(0, 0, 320, 70)
+    // this.ctx.fillStyle = `rgba(0, 0, 0, ${1 / (this.displayDuration / 20)})`
+    // this.ctx.fillRect(0, 0, this.width, this.height)
+    // this.ctx.globalAlpha = 1;
+    // this.ctx.fillRect(0, 0, 320, 70)
     this.count++
   }
 }
