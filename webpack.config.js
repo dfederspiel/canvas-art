1868d16384c800f43b45458843a8bad838601429
David Federspiel <david@codefly.ninja>
2022-08-07 11:51:51 -0400

    update build to include stylesheet

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,38 @@
-const { resolve } = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin')
-const CopyPlugin = require("copy-webpack-plugin")
+const { resolve } = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.ts',
-  mode: 'development',
+  entry: "./src/index.ts",
+  mode: "development",
   module: {
     rules: [
       {
         test: /\.(t|j)sx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
   },
   output: {
     path: `${__dirname}/dist`,
-    filename: 'bundle.js',
+    filename: "bundle.js",
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   plugins: [
     new HtmlWebPackPlugin({
-      template: resolve(__dirname, './public', 'index.html'),
+      template: resolve(__dirname, "./public", "index.html"),
       // favicon: resolve(__dirname, '../public', 'favicon.ico'),
-      filename: './index.html',
+      filename: "./index.html",
     }),
     new CopyPlugin({
       patterns: [
-        { from: 'public/images', to: 'images' }
-      ]
-    })
-  ]
+        { from: "public/images", to: "images" },
+        { from: "public/styles.css", to: "styles.css" },
+      ],
+    }),
+  ],
 };
